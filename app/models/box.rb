7a6a38ef515ab0d788ec2574675824c710cbc8a4
arieldavis22 <aadavisbusiness@gmail.com>
2020-03-24 14:34:52 -0400

    Box addition

--- app/models/box.rb
+++ app/models/box.rb
@@ -2,13 +2,17 @@ class Box < ApplicationRecord
     has_many :box_cards
     has_many :cards, through: :box_cards
 
-    def random_cards
+    def box_for_common
         arr = []
         self.cards.map do |card|
+            if card.points_worth.between?(5,25)
+                10.times do
+                    arr << card
+                end
+            end
             arr << card
         end
         arr.sample(3)
     end
-
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,28 +15,32 @@ User.create(name: "Ariel", points: 50)
 User.create(name: "Laura", points: 50)
 User.create(name: "Bob", points: 50)
 User.create(name: "Tim", points: 50)
-
+#==============================================common
+Card.create(name: "Coal", points_worth: 5)
+Card.create(name: "Moss Agate", points_worth: 10)
+Card.create(name: "Cubic Zirconia", points_worth: 15)
+Card.create(name: "Smoky Quartz", points_worth: 15)
+Card.create(name: "Lapis Lazuli", points_worth: 20)
+Card.create(name: "Rad Ruby", points_worth: 25)
+#+=================================================uncommon
+Card.create(name: "Moldavite", points_worth: 27)
 Card.create(name: "Lemon Quartz", points_worth: 30)
+Card.create(name: "Labradorite", points_worth: 34)
 Card.create(name: "Amber", points_worth: 40)
 Card.create(name: "Opal", points_worth: 61)
-Card.create(name: "Cubic Zirconia", points_worth: 12)
-Card.create(name: "Tiger's Eye touched by Ghandi", points_worth: 351)
-Card.create(name: "Labradorite", points_worth: 34)
-Cart.create(name: "Smoky Quartz", points_worth: 15)
-Cart.create(name: "Lapis Lazuli", points_worth: 20)
-Cart.create(name: "Moldavite", points_worth: 27)
-Cart.create(name: "Moss Agate", points_worth: 8)
-Card.create(name: "Rad Ruby", points_worth: 23)
-Card.create(name: "Moonstone", points_worth: 803)
-Card.create(name: "Coal", points_worth: 5)
+#==================================================rare
+Card.create(name: "Tiger's Eye touched by Ghandi", points_worth: 350)
+Card.create(name: "Moonstone", points_worth: 800)
 Card.create(name: "Diamond", points_worth: 1000)
 
 Box.create(rarity: "Common", cost: 50)
 Box.create(rarity: "Uncommon", cost: 150)
 Box.create(rarity: "Rare", cost: 1000)
 
-20.times do
-    BoxCard.create(card_id: Card.all.sample.id, box_id: Box.all.sample.id)
+Card.all.each do |c|
+    BoxCard.create(card_id: c.id, box_id: Box.first.id)
+    BoxCard.create(card_id: c.id, box_id: Box.second.id)
+    BoxCard.create(card_id: c.id, box_id: Box.last.id)
 end
 
 5.times do
